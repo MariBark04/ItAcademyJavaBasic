579551b6f0e69d0d2bff5e576b0b7b571a0ed94e
Maria Barkova <mari.barkova.04@bk.ru>
2022-09-14 21:08:33 +0300

    My commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a = 14;
+        float b = 22.4f;
+        int c = 0b101;
+      System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        Car c10 = new Car(10);
+        Car c20 = new Car(20);
+        System.out.println(c10.maxSpeed);
+        System.out.println(c20.maxSpeed);
+
+    }
+}
